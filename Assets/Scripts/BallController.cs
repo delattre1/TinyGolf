6d4638be9a8228fe79dc61699170de1a7dc263e9
DiegoSaragozaSilva <torvalds@linux-foundation.org>
2022-10-28 15:20:21 -0300

    club 75%

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,15 +10,21 @@ public class BallController : MonoBehaviour
     private Rigidbody rb;
     private Vector3 startPos;
     private Vector3 endPos;
+    private Vector3 startForce;
+    private Vector3 endForce;
     private bool isClicked;
     private bool isMoving;
+    private bool isPickingForce;
     private GameObject currentClub;
+    private GameObject newClub;
  
     [SerializeField] int force;
     [SerializeField] GameObject club;
+    [SerializeField] GameObject rotateClub;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask ballLayerMask;
     [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private Vector3 offset;
  
     // Start is called before the first frame update
     void Start()
@@ -33,32 +39,45 @@ public class BallController : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, ballLayerMask) && !isMoving){
             startPos = raycastHit.point;
             isClicked = true;
-            currentClub = Instantiate(club, new Vector3(transform.position.x + 4.85f, transform.position.y, transform.position.z + 1.05f), Quaternion.identity) as GameObject;
+            currentClub = Instantiate(club, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity) as GameObject;
         }
-        if (Input.GetMouseButton(0)){
+        if (Input.GetMouseButton(0) && isClicked){
             Ray raycast = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(raycast, out RaycastHit raycastHitGround, float.MaxValue, groundLayerMask)){
                 endPos = raycastHitGround.point;
-                Vector3 angle = endPos - startPos;
-                Vector3 versor = angle.normalized;
+                Vector3 vector = endPos - startPos;
+                Vector3 versor = vector.normalized;
                 Vector3 tacoPos = startPos + (versor);
                 float force = (endPos - startPos).magnitude;
-                if (force > 20f){
-                    force = 20f;
-                }
-                float i = Mathf.InverseLerp(0f, 20f, force);
-                currentClub.transform.position = new Vector3(tacoPos.x + 4.85f, tacoPos.y + 13.9f, tacoPos.z);
-                Vector3 newDirection = Vector3.RotateTowards(transform.forward, angle, 20f, 0.0f);
-                currentClub.transform.eulerAngles = newDirection;
-                //currentClub.transform.eulerAngles = new Vector3(i * 90f, 0f, 0f);
+                if (force > 20f){force = 20f;}
+                float hitAngle = force*90f/20f;
+                Vector3 newPosition = new Vector3(tacoPos.x, 0.5f, tacoPos.z);
+                currentClub.transform.position = newPosition;
+                Vector3 targetPosition = new Vector3(transform.position.x, currentClub.transform.position.y, transform.position.z);
+                currentClub.transform.LookAt(targetPosition);
             }
         }
         if (Input.GetMouseButtonUp(0) && isClicked){
-            Vector3 _Force = endPos - startPos;
             isClicked = false;
-            rb.AddForce(_Force*-force);
-            isMoving = true;
-            Destroy(currentClub);
+            isPickingForce = true;
+            Ray rayForce_0 = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(rayForce_0, out RaycastHit raycastHit0, float.MaxValue)){startForce = raycastHit0.point;}
+            newClub = Instantiate(rotateClub, new Vector3(currentClub.transform.position.x - 5.1f, currentClub.transform.position.y + 13.75f, currentClub.transform.position.z - 0.2f), Quaternion.identity) as GameObject;
+        }
+        if (isPickingForce){
+            //Destroy(currentClub);
+            Ray rayForce_1 = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(rayForce_1, out RaycastHit raycastHit1, float.MaxValue)){endForce = raycastHit1.point;}
+            Vector3 _Force = endForce - startForce;
+            float force = _Force.magnitude;
+            float hitAngle = force*90f/20f;
+            newClub.transform.rotation = Quaternion.AngleAxis(hitAngle, Vector3.right);
+            if (Input.GetMouseButtonDown(0)){    
+                isMoving = true;
+                rb.AddForce(_Force*-force);
+                isPickingForce = false;
+                Destroy(newClub);
+            }
         }
         if(rb.velocity.magnitude <= .5f){
             isMoving = false;
